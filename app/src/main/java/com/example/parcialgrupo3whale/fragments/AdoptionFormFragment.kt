9a69a7aa26ab1c14dc275235a1fc1f3adef6e7d9
UrsulaMartinez <ursula.martinez27@gmail.com>
2023-11-06 01:21:49 -0300

    feat/adoption-form

--- app/src/main/java/com/example/parcialgrupo3whale/fragments/AdoptionFormFragment.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/fragments/AdoptionFormFragment.kt
@@ -5,11 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+//import android.widget.AdapterView
+//import android.widget.ArrayAdapter
+//import android.widget.AutoCompleteTextView
 import com.example.parcialgrupo3whale.R
+//import android.widget.RadioGroup
 
 class AdoptionFormFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.fragment_adoption_form)
+/*
+        val locations =
+            listOf("Buenos Aires","Cordoba","Entre Rios"
+                ,"Corrientes","Misiones","Formosa","Chaco","Santa fe",
+                "Santiago del estero","Tucumán","Salta","Jujuy","Catamarca","La Rioja",
+                "San Juan", "San Luis","Mendoza","La Pampa","Neuquen","Rio negro",
+                "Chubut","Santa Cruz","Tierra del Fuego")
+*/
+
+        //val autoComplete:AutoCompleteTextView = findViewById(R.id.autocompleteLocation)
+
+        //val adapterLocation = ArrayAdapter(this,R.layout.list_location,locations)
+        //autoComplete.setAdapter(adapterLocation)
+    /*
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+                adapterView, view, i, l ->
+            val locacionSelected = AdapterView.getItemAtPosition(i)
+
+        }
+    */
+
     }
 
     override fun onCreateView(

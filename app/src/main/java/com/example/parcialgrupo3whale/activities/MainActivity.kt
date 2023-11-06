641ec8a3ef066480616f2521a1d344219220cf41
Lautaro Nola <lnola@auntap.com>
2023-11-06 20:34:06 -0300

    add strings values, add logic navigation on main activity

--- app/src/main/java/com/example/parcialgrupo3whale/activities/MainActivity.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/activities/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.parcialgrupo3whale.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.parcialgrupo3whale.R
 import com.example.parcialgrupo3whale.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var navView : NavigationView
+    private lateinit var btmNavView : BottomNavigationView
+    private lateinit var navController : NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUpToolbar()
-        setUpNavigation()
+            setUpToolbar()
+            setUpNavigation()
     }
 
     private fun setUpToolbar(){
@@ -22,6 +37,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
+        drawerLayout = binding.drawerLayout
+        navView = binding.navViewMain
+        btmNavView = binding.btmNavViewMain
+
+        // Configurar ActionBarDrawerToggle para el cajón de navegación
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Configurar la AppBarConfiguration
+        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph)
+            .setOpenableLayout(drawerLayout)
+            .build()
+
+        // Configurar la barra de herramientas y la navegación con NavigationUI
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupWithNavController(btmNavView, navController)
     }
 }

85ae021e7ec4475b9113305ff94b5cf86339186a
Lautaro Nola <lnola@auntap.com>
2023-11-07 00:58:05 -0300

    fix pet api response name

--- app/src/main/java/com/example/parcialgrupo3whale/gateway/model/PetBreedsAPIResponse.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/gateway/model/PetBreedsAPIResponse.kt
@@ -2,7 +2,7 @@ package com.example.parcialgrupo3whale.gateway.model
 
 import com.google.gson.annotations.SerializedName
 
-data class PetAPIResponse(
+data class PetBreedsAPIResponse(
     @SerializedName("message") val message: Map<String, List<String>>,
     @SerializedName("status") val status: String
 )

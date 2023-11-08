421ef0d7a56776f3bd40fe7cbb66109d2b422609
Juan I Pulleiro <juan.i.pulleiro@gmail.com>
2023-11-07 21:15:12 -0300

    funcion de la flecha back + visibilidad navView

--- app/src/main/java/com/example/parcialgrupo3whale/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/fragments/ProfileFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import com.example.parcialgrupo3whale.R
+import com.example.parcialgrupo3whale.activities.MainActivity
+import com.example.parcialgrupo3whale.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,5 +29,24 @@ class ProfileFragment : Fragment() {
         textView.text = argumentValue
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val navController = binding.root.findNavController()
+
+        binding.arrowBackImg.setOnClickListener{
+
+            if (navController != null) navController.popBackStack(R.id.nav_graph_xml, false)
+
+            if (activity is MainActivity) {
+                (activity as MainActivity).setBottomNavViewVisibility(View.VISIBLE)
+                (activity as MainActivity).supportActionBar?.show()
+            }
+
+            navController.popBackStack()
+            true
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
 }

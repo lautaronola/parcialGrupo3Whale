9440bf7e18bcf19a449dc39cc1da20067ef93a88
Juan I Pulleiro <juan.i.pulleiro@gmail.com>
2023-11-09 11:32:41 -0300

    Resolve conflicts

--- app/src/main/java/com/example/parcialgrupo3whale/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/fragments/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.example.parcialgrupo3whale.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import com.example.parcialgrupo3whale.R
 import com.example.parcialgrupo3whale.activities.MainActivity
 import com.example.parcialgrupo3whale.databinding.FragmentProfileBinding
@@ -14,8 +18,16 @@ import com.example.parcialgrupo3whale.databinding.FragmentProfileBinding
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -27,6 +39,13 @@ class ProfileFragment : Fragment() {
         val argumentValue = arguments?.getString("userName")
         val textView: TextView = view.findViewById(R.id.nameProfile)
 
+        button = view.findViewById(R.id.upload_photo)
+        imageView = view.findViewById(R.id.profile_image)
+
+        button.setOnClickListener {
+            pickImageGallery()
+        }
+
         textView.text = argumentValue
 
         return view
@@ -36,7 +55,7 @@ class ProfileFragment : Fragment() {
 
         val navController = binding.root.findNavController()
 
-        binding.arrowBackImg.setOnClickListener{
+        binding.arrowBackImg.setOnClickListener {
 
             if (navController != null) navController.popBackStack(R.id.nav_graph_xml, false)
 
@@ -49,5 +68,17 @@ class ProfileFragment : Fragment() {
             true
         }
         super.onViewCreated(view, savedInstanceState)
+    }
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == AppCompatActivity.RESULT_OK) {
+            imageView.setImageURI(data?.data)
+        }
     }
 }

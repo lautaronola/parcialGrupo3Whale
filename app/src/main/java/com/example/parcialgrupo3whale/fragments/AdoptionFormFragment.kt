2e0a86aa54f32c6fbd5447cf474f5882da7ddad7
Juan I Pulleiro <juan.i.pulleiro@gmail.com>
2023-11-09 18:08:58 -0300

    Revert "Armando logica para guardar el pet creado"
    This reverts commit 6d86acd3714e93d5377dd18d5324280119fe68fc.

--- app/src/main/java/com/example/parcialgrupo3whale/fragments/AdoptionFormFragment.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/fragments/AdoptionFormFragment.kt
@@ -1,91 +1,49 @@
 package com.example.parcialgrupo3whale.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
+import androidx.viewbinding.ViewBindings
 //import android.widget.AdapterView
 //import android.widget.ArrayAdapter
 //import android.widget.AutoCompleteTextView
 import com.example.parcialgrupo3whale.R
-import com.example.parcialgrupo3whale.database.dao.PetDao_Impl
-import com.example.parcialgrupo3whale.databinding.FragmentAdoptionFormBinding
-import com.example.parcialgrupo3whale.entities.PetEntity
-
 //import android.widget.RadioGroup
+
 
 class AdoptionFormFragment : Fragment() {
 
-    private lateinit var addButton: Button
-    private lateinit var nameEditText: EditText
-    private lateinit var ageEditText: EditText
-    private lateinit var weightEditText: EditText
-    private lateinit var descriptionEditText: EditText
-    private lateinit var genderRadioGroup: RadioGroup
-    private lateinit var locationAutoCompleteTextView: AutoCompleteTextView
-    private lateinit var raceAutoCompleteTextView: AutoCompleteTextView
-    private lateinit var subraceAutoCompleteTextView: AutoCompleteTextView
+    val locations =
+        listOf("Buenos Aires","Cordoba","Entre Rios"
+            ,"Corrientes","Misiones","Formosa","Chaco","Santa fe",
+            "Santiago del estero","Tucumán","Salta","Jujuy","Catamarca","La Rioja",
+            "San Juan", "San Luis","Mendoza","La Pampa","Neuquen","Rio negro",
+            "Chubut","Santa Cruz","Tierra del Fuego").toList()
+
+    val race = listOf("Generica","Golden","Chihuahua","Bulldog","Beagle","Otros").toList()
+    val Subrace = listOf("Generica","Golden","Chihuahua","Bulldog","Beagle","Otros").toList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+      }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_adoption_form, container, false)
-
-        // Initialize your view references
-        nameEditText = view.findViewById(R.id.textEditNamePet)
-        ageEditText = view.findViewById(R.id.textEditAgePet)
-        weightEditText = view.findViewById(R.id.textEditWeightPet)
-        descriptionEditText = view.findViewById(R.id.textEditDescriptionPet)
-        genderRadioGroup = view.findViewById(R.id.radioGroupGenres)
-        locationAutoCompleteTextView = view.findViewById(R.id.autocompleteLocation)
-        raceAutoCompleteTextView = view.findViewById(R.id.autocompleteRace)
-        subraceAutoCompleteTextView = view.findViewById(R.id.autocompleteSubrace)
-        addButton = view.findViewById(R.id.BtnAdoptionAdd)
-
-        // Set up click listener for the add button
-        addButton.setOnClickListener {
-            val name = nameEditText.text.toString()
-            val age = ageEditText.text.toString()
-            val weight = weightEditText.text.toString()
-            val description = descriptionEditText.text.toString()
-            val gender = genderRadioGroup.checkedRadioButtonId == R.id.radioButtonFemale
-            val location = locationAutoCompleteTextView.text.toString()
-            val race = raceAutoCompleteTextView.text.toString()
-            val subrace = subraceAutoCompleteTextView.text.toString()
-            val owner = arguments?.getString("userName").toString()
-
-            // Crear una instancia de PetEntity
-            val pet = PetEntity(name, age, weight, description, gender, location, owner, race, subrace)
-
-            // Loggear la información de la mascota
-            Log.d("PetInfo", pet.toString())
-
-            // Agregar la mascota a la lista
-            db.petDao().insertPet(pet)
-        }
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_adoption_form, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setDataAdoptionPet()
     }
 
     private fun setDataAdoptionPet() {
-        //binding
+        val namePet =
     }
 }

11cdd91b4acef49f82e223971d8d8588b147d866
Matias MAZZUCCHELLI <matias.mazzucchelli@naranjax.com>
2023-11-09 20:51:37 -0300

    Filter and search bar impl finished

--- app/src/main/java/com/example/parcialgrupo3whale/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/parcialgrupo3whale/fragments/HomeFragment.kt
@@ -13,6 +13,7 @@ import com.example.parcialgrupo3whale.adapters.PetsListHomeAdapter
 import com.example.parcialgrupo3whale.database.dao.WhaleDatabase
 import com.example.parcialgrupo3whale.database.entities.PetEntity
 import com.example.parcialgrupo3whale.listener.OnDetailFragmentClickListener
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.snackbar.Snackbar
 
 class HomeFragment : Fragment(), OnDetailFragmentClickListener {
@@ -21,6 +22,8 @@ class HomeFragment : Fragment(), OnDetailFragmentClickListener {
     private lateinit var pets: List<PetEntity>
     private lateinit var linearLayoutManager : LinearLayoutManager
     private lateinit var petsListHomeAdapter : PetsListHomeAdapter
+    private lateinit var searchView: androidx.appcompat.widget.SearchView
+    private lateinit var chipGroupFilters: ChipGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +33,34 @@ class HomeFragment : Fragment(), OnDetailFragmentClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false)
         recyclerPets = view.findViewById(R.id.recyclerPets)
+
+        searchView = view.findViewById(R.id.searchBar)
+        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { searchForPets(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { searchForPets(it) }
+                return true
+            }
+        })
+
+        chipGroupFilters = view.findViewById(R.id.chipGroupFilters)
+        setupChipGroupListener()
+
         return view
+    }
+
+    private fun initializeRecyclerView(petsList: List<PetEntity>) {
+        recyclerPets.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        petsListHomeAdapter = PetsListHomeAdapter(petsList, this, WhaleDatabase.getWhaleDatabase(requireContext()))
+        recyclerPets.layoutManager = linearLayoutManager
+        recyclerPets.adapter = petsListHomeAdapter
     }
 
     override fun onStart() {
@@ -45,13 +72,19 @@ class HomeFragment : Fragment(), OnDetailFragmentClickListener {
         linearLayoutManager = LinearLayoutManager(context)
 
         val db = WhaleDatabase.getWhaleDatabase(requireContext())
-        if (db != null) {
-            pets = db.petDao().getAllPets()
-        }
+        pets = db?.petDao()?.getAllPets() ?: emptyList()
 
         petsListHomeAdapter = PetsListHomeAdapter(pets, this, db)
         recyclerPets.layoutManager = linearLayoutManager
         recyclerPets.adapter = petsListHomeAdapter
+        initializeRecyclerView(pets)
+    }
+
+    private fun searchForPets(query: String) {
+        // Filtra la lista de mascotas basada en la raza
+        val filteredList = pets.filter { it.breed.contains(query, ignoreCase = true) }
+        // Actualiza el adaptador con la lista filtrada
+        petsListHomeAdapter.updatePetsList(filteredList)
     }
 
    override fun onViewItemDetail(pet: PetEntity){
@@ -59,4 +92,20 @@ class HomeFragment : Fragment(), OnDetailFragmentClickListener {
        this.findNavController().navigate(action)
        Snackbar.make(view, pet.toString(), Snackbar.LENGTH_SHORT).show()
    }
+
+    private fun setupChipGroupListener() {
+        chipGroupFilters.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId) {
+                R.id.chip_golden -> searchForPets("Golden")
+                R.id.chip_salchicha -> searchForPets("Salchicha")
+                R.id.chip_terrier -> searchForPets("Terrier")
+                else -> displayAllPets()
+            }
+        }
+    }
+
+    private fun displayAllPets() {
+        petsListHomeAdapter.updatePetsList(pets)
+    }
+
 }
